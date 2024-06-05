51db2d5996dc79be2c4d97fe5cb3c5a791f4ee78
Guilherme <bittenkourt.gmf@gmail.com>
2024-06-05 16:44:31 -0300

    New updates

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const testimonials = document.querySelectorAll('.testimonial');
-    const dots = document.querySelectorAll('.dot');
-    let currentIndex = 0;
-
-    function showTestimonial(index) {
-        testimonials.forEach((testimonial, i) => {
-            testimonial.classList.remove('active');
-            dots[i].classList.remove('active');
-            if (i === index) {
-                testimonial.classList.add('active');
-                dots[i].classList.add('active');
-            }
-        });
-    }
-
-    function nextTestimonial() {
-        currentIndex = (currentIndex + 1) % testimonials.length;
-        showTestimonial(currentIndex);
-    }
-
-    dots.forEach(dot => {
-        dot.addEventListener('click', (event) => {
-            const index = parseInt(event.target.getAttribute('data-index'));
-            currentIndex = index;
-            showTestimonial(currentIndex);
-        });
-    });
-
-    showTestimonial(currentIndex);
-
-    setInterval(nextTestimonial, 5000); // Muda o depoimento a cada 5 segundos
-});
+window.addEventListener('load', function () {
+    init();
+   });
+   
+   function init(){
+       let toolTimeline = new TimelineMax();
+       let duration = .9;
+       toolTimeline.from('h1', duration, {
+           opacity:0,
+           // scale:10,
+           ease:Linear.easeIn
+       }, .8)
+       toolTimeline.staggerFrom('.boxes__box', duration, {
+           opacity:0,
+           scale:0,
+           ease:Back.easeInOut
+       }, .1 , .2);
+       
+       toolTimeline.to('.boxes img', duration, {
+           opacity:1,		
+           right:0,
+           ease:Back.easeOut
+       }, .4);
+       
+       toolTimeline.staggerTo('li', duration, {
+           
+           top:0,
+           ease:Back.easeOut
+       }, .1, .9);
+       
+   };
